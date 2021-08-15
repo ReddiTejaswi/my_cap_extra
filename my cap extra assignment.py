3195ad4a0165ffa23f5b80fcaf7e4128400d02ca
ReddiTejaswi <83908841+ReddiTejaswi@users.noreply.github.com>
2021-08-15 20:26:12 +0530

    string methods,list methods

--- my cap extra assignment.py
+++ my cap extra assignment.py
@@ -68,3 +68,51 @@ txt3 = "My name is {}, I'm {}".format("Tej",36)
 print(txt1)
 print(txt2)
 print(txt3)
+
+
+
+
+
+
+
+"""list methods"""
+
+l1=["teju","india",1,2,3]
+l1.append(4)
+print(l1)#Adds an element at the end of the list
+
+x = l1.copy()
+print(x)
+#copy() method returns a copy of the specified list.
+
+
+x = l1.count("india")
+print(x)
+
+fruits=["apple","banana","cherry"]
+l1.extend(fruits)
+print(l1)
+
+print(fruits.index("cherry"))
+
+x=fruits.insert(0,"mango")
+print(fruits)
+
+fruits.pop(1)
+print(fruits)
+
+fruits.remove("banana")
+print(fruits)
+
+l1.reverse()
+print(l1)
+
+fruits.sort()
+print(fruits)
+
+l1.clear() #Removes all the elements from the list
+print(l1)
+
+
+
+
